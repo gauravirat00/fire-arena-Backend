ada97028a57540be8540f5ac889d9d0515444d6a
gauravirat00 <beyzalrgn123@gmail.com>
2025-07-04 16:28:01 +0530

    Update status.js

--- routes/status.js
+++ routes/status.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// GET /user/
+router.get('/', (req, res) => {
+  res.json({
+    status: "🟢 | GOOD",
+    DB: {
+      connected: global.isMongoConnected || False
+    },
+    uptime: 0
+  });
+});
+
+module.exports = router;
 
