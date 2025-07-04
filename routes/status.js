cd2d2f4200dd0166ebe5b586bc7342b9c56bfc32
gauravirat00 <beyzalrgn123@gmail.com>
2025-07-04 17:05:29 +0530

    Update status.js

--- routes/status.js
+++ routes/status.js
@@ -3,12 +3,16 @@ const router = express.Router();
 
 // GET /user/
 router.get('/', (req, res) => {
+  let uptimeInMs = Date.now() - global.startTime;
+  let parsedUptime = ms(uptimeInMs);
+  let uptime = `${parsedUptime.days} D, ${parsedUptime.hours} H, ${parsedUptime.minutes} M, ${parsedUptime.seconds} S`;
+  
   res.json({
     status: "🟢 | GOOD",
     DB: {
       connected: global.isMongoConnected || false
     },
-    uptime: 0
+    uptime 
   });
 });
 
